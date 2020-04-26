027859a9e253342794f4c179395a808166a4041f
David Kanenwisher <d.kanen@gmail.com>
2020-04-26 17:13:16 -0500

    Seed a user with a group, trip, and catchables.

--- app/User.php
+++ app/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Group');
     }
+
+    public function catchables()
+    {
+        return $this->groups();
+    }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Catchable;
+use App\Group;
+use App\Trip;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class, 1)->create(['api_token' => 'authenticate-me']);
+        $user = factory(User::class, 1)->create(['api_token' => 'authenticate-me'])->first();
+        $group = factory(Group::class)->create();
+        $trip = factory(Trip::class)->create([
+            'name' => 'Beach Island',
+            'group_id' => $group->id
+        ]);
+        factory(Catchable::class)->create([
+            'name' => 'Squid',
+            'trip_id' => $trip->id
+        ]);
+        
+        $user->groups()->attach($group);
     }
 }
